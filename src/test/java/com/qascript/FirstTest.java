9b8d6c89a63925b12ee8bd24dd9632b7cdc91d45
Jay1p <82936665+Jay1p@users.noreply.github.com>
2023-04-12 16:38:23 +0100

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTest {
